6d0d516c170e53058d9bb358a10679b1afb3a4f6
naufalbintoro749 <naufalzero749@gmail.com>
2025-12-13 03:05:02 +0700

    Update layanan.js

--- layanan.js
+++ layanan.js
@@ -1,21 +1,12 @@
-import { readFileSync, existsSync } from "fs";
+let layananData = [
+  "Cuci AC",
+  "Service Ringan",
+  "Isi Freon",
+  "Service Besar",
+  "Tambal Bocor",
+  "Maintenance AC Bulanan"
+];
 
 export default function handler(req, res) {
-  const path = "/tmp/layanan.json";
-
-  if (existsSync(path)) {
-    const raw = readFileSync(path, "utf8");
-    return res.status(200).json(JSON.parse(raw));
-  }
-
-  return res.status(200).json({
-    layanan: [
-      "Cuci AC",
-      "Service Ringan",
-      "Isi Freon",
-      "Tambal Kebocoran",
-      "Service Besar",
-      "Perbaikan Total"
-    ]
-  });
+  res.status(200).json({ layanan: layananData });
 }
